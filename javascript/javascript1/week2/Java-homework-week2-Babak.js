9411789616efdc86531db0a18d1f771cff65f8cf
Babak <babak.bashirzadeh@gmaile.com>
2025-02-10 18:10:00 +0100

    I made a system for show person what day the event should held

--- javascript/javascript1/week2/Java-homework-week2-Babak.js
+++ javascript/javascript1/week2/Java-homework-week2-Babak.js
@@ -1,3 +1,4 @@
+/*Flight booking fullname function*/
 function getFullName(firstName, lastName) {
     if (useFormalName) {
         return `Lord ${firstName} ${lastName}`;
@@ -9,3 +10,5 @@ console.log(useFormalName("Babak", "Bashirzadeh", true));
 
 /*For the question about women, we should define a option that will return the gender of the person. after that we can define a new
 function that will return the formal name of the person based on the gender.*/
+
+/*event application*/
