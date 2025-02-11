3d3a7c898f887401e4c338d592cb1eb0bbc7ce84
Babak <babak.bashirzadeh@gmaile.com>
2025-02-11 13:08:58 +0100

    i made a function to add students name based on 4 rules, i used .include to check students name inside the array  and .push to push student name inside the array if all conditions right.

--- javascript/javascript1/week2/Java-homework-week2-Babak.js
+++ javascript/javascript1/week2/Java-homework-week2-Babak.js
@@ -12,3 +12,24 @@ console.log(useFormalName("Babak", "Bashirzadeh", true));
 function that will return the formal name of the person based on the gender.*/
 
 /*event application*/
+function getEventWeekday(eventDay) {
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const today = new Date();
+    const eventDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + eventDay);
+    return weekdays[eventDate.getDay()];
+}
+console.log(getEventWeekday(9));
+
+/*Weather wear*/
+function wearType (temperature) {
+    if (temperature < 0) {
+        return "Wear a coat, hat, scarf and gloves";
+    } else if (temperature < 10) {
+        return "Wear a coat and bring a hat";
+    } else if (temperature < 20) {
+        return "Wear a coat";
+    } else (temperature < 30) {
+        return "Get ready for summer";
+    }
+}
+console.log(wearType(15));
