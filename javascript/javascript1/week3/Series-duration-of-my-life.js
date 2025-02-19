0aee7bf9e77b57e0755301ded190b13c99349812
Babak <babak.bashirzadeh@gmaile.com>
2025-02-19 18:31:12 +0100

    i have done the 3th homework, please check my codes, thank you

---
+++ javascript/javascript1/week3/Series-duration-of-my-life.js
@@ -0,0 +1,25 @@
+const seriesDurations = [
+    {
+      title: "Game of thrones",
+      days: 3,
+      hours: 1,
+      minutes: 0,
+    },
+    {
+      title: "Rick and Morty",
+      days: 1,
+      hours: 8,
+      minutes: 58,
+    },
+    {
+      title: "devs",
+      hours: 6,
+      minutes: 56,
+    },
+    {
+        title: "Dexter",
+        days: 4,
+        hours: 23,
+        minutes: 22,
+      },
+  ];
