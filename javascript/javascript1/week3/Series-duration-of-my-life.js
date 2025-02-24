6f77d1de11b105537f47be8984c197732d64e8a1
Babak <babak.bashirzadeh@gmaile.com>
2025-02-24 12:52:55 +0100

    i make last part again

--- javascript/javascript1/week3/Series-duration-of-my-life.js
+++ javascript/javascript1/week3/Series-duration-of-my-life.js
@@ -13,6 +13,7 @@ const seriesDurations = [
     },
     {
       title: "devs",
+      days: 0,
       hours: 6,
       minutes: 56,
     },
@@ -23,3 +24,20 @@ const seriesDurations = [
         minutes: 22,
       },
   ];
+  function timeSpend () {
+    const averageLifeYears = 80;
+    const averageLifeMinutes = averageLifeYears * 365 * 24;
+    for (let series of seriesDurations) {
+      const title = series.title;
+      const days = series.days;
+      const hours = series.hours;
+      const minutes = series.minutes;
+      const totalTimeSpend = (days * 24 + hours) * 60 + minutes;
+      const percentageOfLife = (totalTimeSpend/averageLifeMinutes) * 100;
+      console.log (`${title} took ${percentageOfLife.toFixed(2)}% of 80 years old person`);
+      let totalTime =+ percentageOfLife.toFixed(2);
+      console.log ("In total that is " + totalTime + " of your life")
+    }  
+    
+  }
+  timeSpend();
